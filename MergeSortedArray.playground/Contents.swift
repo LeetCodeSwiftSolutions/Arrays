06452b5467f20c7ba34d7cc82a0520413d9f1288
Miguel Torres <99153283+MrPicudo@users.noreply.github.com>
2025-11-21 12:06:33 -0600

    Merge sorted array - Solución optimizada
    - Usamos tres punteros (índices) para controlar el recorrido y el acomodo de los elementos en el arreglo.
    - Vamos recorriendo del último elemento al primero, para acomodar los números en orden creciente.

--- MergeSortedArray.playground/Contents.swift
+++ MergeSortedArray.playground/Contents.swift
@@ -3,8 +3,9 @@ Se deben combinar dos arreglos enteros ordenados, nums1 y nums2, usando solo sus
 
 You must merge two sorted integer arrays, nums1 and nums2, using only their first m and n elements respectively, producing a single sorted array stored directly in nums1, which has total space m + n. The trailing zeros in nums1 should be ignored. The goal is to merge both arrays in non-decreasing order in O(m + n) time. */
 
-
+///
 /// Solución original
+///
 func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
     // Si nums1 no contiene elementos propios, lo cambiamos por nums2
     guard m != 0 else { nums1 = nums2; return }
@@ -21,16 +22,43 @@ func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
 }
 
 ///
-/// Pruebas
+/// Pruebas de solución original
 ///
 
 var nums1: [Int] = [1,2,3,17,21,45,93,0,0,0,0,0]
 var nums2: [Int] = [-89,-17,19,58,77]
 merge(&nums1, 7, nums2, 5)
 
-/*
- var nums1: [Int] = [1,2,3,0,0,0]
- var nums2: [Int] = [2,5,6]
- merge(&nums1, 3, nums2, 3)
- */
 
+///
+/// Solución optimizada
+///
+func optimizedMerge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    var i = m - 1        // Último elemento válido en nums1
+    var j = n - 1        // Último elemento en nums2
+    var k = m + n - 1    // Posición para escribir en nums1
+
+    // Mientras queden elementos en nums2
+    while j >= 0 {
+        // Si el último elemento válido de nums1 es mayor que el de nums2, lo acomodamos en la posición k de nums1
+        if i >= 0 && nums1[i] > nums2[j] {
+            nums1[k] = nums1[i]
+            i -= 1
+        } else {
+            // Si no, acomodamos el último elemento válido de nums2 en la posición k de nums1.
+            nums1[k] = nums2[j]
+            j -= 1
+        }
+        // Recorremos la posición para insertar en nums1.
+        k -= 1
+    }
+    // Si j < 0, los elementos restantes de nums1 ya están en su lugar.
+}
+
+///
+/// Pruebas de solución optimizada
+///
+
+var n1: [Int] = [1,2,3,0,0,0]
+let n2: [Int] = [2,5,6]
+optimizedMerge(&n1, 3, n2, 3)
